9467db2ae3eb93b6efc2a833f9b17e9aca8897be
PanYJ <1025318325@qq.com>
2018-07-10 16:18:34 +0800

    new classes

--- Scrapy.py
+++ Scrapy.py
@@ -8,3 +8,27 @@ from scrapy.item import Item,Field
 class StackItem(Item):
     title=Field()
     url = Field()
+
+from scrapy import Spider
+from scrapy.selector import Selector
+
+from stack.items import StackItem
+
+
+class StackSpider(Spider):
+    name = "stack"
+    allowed_domains = ["stackoverflow.com"]
+    start_urls = [
+        "http://stackoverflow.com/questions?pagesize=50&sort=newest",
+    ]
+
+    def parse(self, response):
+        questions = Selector(response).xpath('//div[@class="summary"]/h3')
+
+        for question in questions:
+            item = StackItem()
+            item['title'] = question.xpath(
+                'a[@class="question-hyperlink"]/text()').extract()[0]
+            item['url'] = question.xpath(
+                'a[@class="question-hyperlink"]/@href').extract()[0]
+            yield item
